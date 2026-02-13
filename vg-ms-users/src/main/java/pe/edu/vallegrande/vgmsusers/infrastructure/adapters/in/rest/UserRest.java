4812c7e73f17d09148fe46a361d703ad1fc7844a
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2026-02-13 09:34:07 -0500

    feat: add Swagger `@Parameter` annotation import.

--- vg-ms-users/src/main/java/pe/edu/vallegrande/vgmsusers/infrastructure/adapters/in/rest/UserRest.java
+++ vg-ms-users/src/main/java/pe/edu/vallegrande/vgmsusers/infrastructure/adapters/in/rest/UserRest.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Parameter;
 import pe.edu.vallegrande.vgmsusers.application.dto.common.ApiResponse;
 import pe.edu.vallegrande.vgmsusers.application.dto.request.CreateUserRequest;
 import pe.edu.vallegrande.vgmsusers.application.dto.request.UpdateUserRequest;
