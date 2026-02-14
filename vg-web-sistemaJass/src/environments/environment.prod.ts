e49d2ad488307d7baded51128b4387bf78167a1f
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2026-02-14 06:51:26 -0500

    feat: add Docker configuration and Nginx setup for Angular application deployment

--- vg-web-sistemaJass/src/environments/environment.prod.ts
+++ vg-web-sistemaJass/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
      production: true,
-     apiUrl: 'https://api.jassdigital.pe/api',
-     evolutionApiUrl: 'https://evolution.jassdigital.pe'
+     apiUrl: '/jass/SistemaJass/api',
+     evolutionApiUrl: '/jass/SistemaJass/evolution',
+     evolutionApiKey: 'JASS_EVOLUTION_KEY_2024'
 };
